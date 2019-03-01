1682528840a7a1f837dc905d74792d09993e6db9
Raju Lamsal <rajulamsal62@gmail.com>
2019-03-01 18:20:07 +0545

    day02 started

---
+++ challenge/day02/index.php
@@ -0,0 +1,21 @@
+<html>
+    <form action="index.php">
+        <select name="cng_frm" id="frm">
+            <option value="dgr">Degree</option>
+            <option value="fr">Fahrenheit</option>
+            <option value="cel">Celcius</option>
+        </select>
+            TO
+            <select name="cng_to" id="to">
+            <option value="dg">Degree</option>
+            <option value="far">Fahrenheit</option>
+            <option value="celc">Celcius</option>
+        </select>
+        <input type="number" name="Temparature"/>
+        <label for="cel">Celsius</label>
+        <button type="submit" name="convert">Convert Temprature</button>
+    </form>
+</html>
+
+<script>
+</script>
