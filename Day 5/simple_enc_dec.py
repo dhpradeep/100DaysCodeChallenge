731b0ba72ac79fcec39b0ca3cefe0f2bebc301bb
dhpradeep <dhpradeep25@gmail.com>
2019-03-04 18:09:59 +0545

    Modify encrypt decrypt algo

--- Day 5/simple_enc_dec.py
+++ Day 5/simple_enc_dec.py
@@ -1,12 +1,6 @@
 # Python simple encryption description program.
 import sys
 
-import itertools
-
-def alternate(xs, ys):
-    head = itertools.chain.from_iterable(zip(xs, ys))
-    return itertools.chain(head, xs[len(ys):], ys[len(xs):])
-
 def helpMessage():
     print("\nHelp message")
     print("encrypter [option] [value]")
@@ -15,60 +9,27 @@ def helpMessage():
     print("\t--decrypt or -D => decrypt")
     print("\tFor example: 'encrypter -E pradeep'")
 
-def hashcode():
-    return list('my-hashcode')
+def encrypt(key, msg):
+    encryped = []
+    for i, c in enumerate(msg):
+        key_c = ord(key[i % len(key)])
+        msg_c = ord(c)
+        encryped.append(chr((msg_c + key_c) % 127))
+    return ''.join(encryped)
 
-def encrypt(normalText,val):
-    # if(val >= 1):
-    #     textlen = len(normalText)
-    #     hashcodeString = hashcode()
-    #     evenString = []
-    #     oddString = []
-    #     for i in range(textlen):
-    #         if i % 2 != 0:
-    #             evenString.insert(i,normalText[i])
-    #         else:
-    #             oddString.insert(i,normalText[i])
-
-    #     cypherText = list(alternate(evenString,oddString))
-    #     cypherText = list(alternate(cypherText,hashcodeString))
-        
-    #     cypherText = [''.join(i for i in cypherText)]
-    #     cypherText = str(cypherText[0])
-    #     val = val -1
-    #     print(cypherText)
-    #     return(encrypt(cypherText,val))
-    # else:
-    #     return normalText
-    return normalText[::-1]
-    
-    
-def decrypt(cypherText,val):
-    # if(val >= 1):
-    #     hashcodeString = hashcode()
-    #     textlen = len(cypherText)
-    #     normalText = [cypherText]
-    #     normalText = list(alternate(normalText,hashcodeString))
-    #     evenString = []
-    #     oddString = []
-    #     # for i in range(textlen):
-    #     #     if i % 2 != 0:
-    #     #         evenString.insert(i,normalText[i])
-    #     #     else:
-    #     #         oddString.insert(i,normalText[i])
-    #     # normalText = list(alternate(evenString,oddString))
-    #     print(normalText)
-    #     return(decrypt(cypherText,val))
-    # else:
-    #     return cypherText
-    return cypherText[::-1]
-
-    
+def decrypt(key, encryped):
+    msg = []
+    for i, c in enumerate(encryped):
+        key_c = ord(key[i % len(key)])
+        enc_c = ord(c)
+        msg.append(chr((enc_c - key_c) % 127))
+    return ''.join(msg)
 
 arg1, arg2 = sys.argv[1:3]
+key = "This_is_my_key"
 if(arg1.lower() == '--encrypt' or arg1.upper() == '-E'):
-    print(encrypt(arg2,1))
+    print(encrypt(key,arg2))
 elif(arg1.lower() == '--decrypt' or arg1.upper() == '-D'):
-    print(decrypt(arg2,1))
+    print(decrypt(key,arg2))
 else:
     helpMessage()
