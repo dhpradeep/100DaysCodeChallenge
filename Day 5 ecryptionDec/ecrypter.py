65ca6293b1c28515ea20768386728a342f6ec7bc
PrabhuGurung <40710025+PrabhuGurung@users.noreply.github.com>
2019-03-04 12:10:41 +0545

    day 5 encrypter

--- Day 5 ecryptionDec/ecrypter.py
+++ Day 5 ecryptionDec/ecrypter.py
@@ -1,29 +1,42 @@
 import sys
+import base64
 
-def encoder(val):
-    return (map(bin,bytearray(val, 'utf8')))
+key = "prabhu"
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
-def myecryptkey():
-    return(encoder("Prabhu Gurung"))
-
-def encrypt(value):
-    binText = encoder(value)
-    myeconderKey = myecryptkey()
-    
-    print (binText)
-
-
-  #return cypherText
-
-
-arg1,arg2 = sys.argv[1:3]
-
-if arg1.lower() == "--e":
-    encrypt(arg2)
-elif arg1.lower() == "--d":
-    decrypt(arg2)
-else: 
-    print("bad argument \n : help \n encrypter [option] [value] \n option : \n --e : encrypter \n --d : decrypter")
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
 
 
+while True:
+    try:
+        arg1,arg2 = sys.argv[1:3]
+    except:
+        print(": help \n [option] [value] \n option : \n --e : encrypter \n --d : decrypter")
+        arg1,arg2 = input().split()
+        
+
+    if arg1.lower() == "--e":
+        ec =encode(key, arg2)
+        print(ec)
+    elif arg1.lower() == "--d":
+        dc = decode(key, arg2)
+        print(dc)
+    else: 
+        print("bad argument \n : help \n encrypter [option] [value] \n option : \n --e : encrypter \n --d : decrypter")
+
+    if input("Press E for exit: ").lower() == "e":
+        exit()
 
